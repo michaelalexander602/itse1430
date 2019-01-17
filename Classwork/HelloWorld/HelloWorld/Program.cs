7ba15b158a5cf7987545c212461f9ec4eee5003d
michael.alexander602 <michael.alexander602@NEITD-123509>
2019-01-16 21:44:15 -0600

    a

---
+++ Classwork/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            string name;
+
+            Console.WriteLine("Enter your name: ");
+            name = Console.ReadLine();
+
+            Console.Write("Hello " + name);
+            Console.WriteLine(name);
+
+
+        }
+    }
+}
