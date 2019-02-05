217730c845771dbb0ca3ad6ed2fbdd98c7ea4769
michael.alexander602 <michael.alexander602@NEITD-123509>
2019-02-04 21:37:31 -0600

    2-4

---
+++ Classwork/GameManager/GameManager.Host.Winforms/MainForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameManager.Host.Winforms
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+
+            LoadUI();
+        }
+
+        void LoadUI ()
+        {
+            Game game = new Game();
+
+            game.Name = "kh3";
+            game.Price = 59.99M;
+        }
+    }
+}
---
+++ Classwork/GameManager/GameManager/Game.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameManager
+{
+    /// <summary>Represents a game.</summary>
+     public class Game
+    {
+        /// <summary>Name of the game.</summary>
+        public string Name;
+
+        /// <summary>Publisher of the game</summary>
+        public string Publisher;
+        public decimal Price;
+        public bool Owned;
+        public bool Completed;
+    }
+}
