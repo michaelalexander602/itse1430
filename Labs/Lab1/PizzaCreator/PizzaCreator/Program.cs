871daea48775fd45b1628dbdcf007923dd475c37
khayoz <msa817@yahoo.com>
2019-02-06 18:24:28 -0600

    lab1 2-6

---
+++ Labs/Lab1/PizzaCreator/PizzaCreator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaCreator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
