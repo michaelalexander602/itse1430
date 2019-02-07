0980009ba43cd9d7a9dcfad0551998c4593b8026
khayoz <msa817@yahoo.com>
2019-02-07 15:42:27 -0600

    lab1 2-7

--- Labs/Lab1/PizzaCreator/PizzaCreator/Program.cs
+++ Labs/Lab1/PizzaCreator/PizzaCreator/Program.cs
@@ -10,6 +10,29 @@ namespace PizzaCreator
     {
         static void Main(string[] args)
         {
+            int choice = 0;
+            do
+            {
+                Console.WriteLine("1. New Order");
+                Console.WriteLine("2. Modify Order");
+                Console.WriteLine("3. Display Order");
+                Console.WriteLine("4. New Order");
+                Console.Write("Make your selection by entering a number (1-4): ");
+
+                while (!int.TryParse(Console.ReadLine(), out choice))
+                {
+                    Console.WriteLine("Error");
+                }
+
+                //choice = int.Parse(Console.ReadLine());
+                Console.Write(choice);
+
+                while (choice < 1 || choice > 4)
+                {
+                    Console.WriteLine("Invalid Selection! Please enter a number (1-4)");
+                    int.TryParse(Console.ReadLine(), out choice);
+                }
+            } while (choice != 4);
         }
     }
 }
