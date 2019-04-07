4f05054adc0d8a497473705a837f459e07ab3bb3
khayoz <msa817@yahoo.com>
2019-04-07 18:55:56 -0500

    47

---
+++ Labs/Lab3/ContactManager.UI/ContactManager.Business/Contact.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactManager.Business
+{
+    public class Contact
+    {
+        public string Name
+        {
+            get { return _name ?? ""; }
+            set { _name = value ?? ""; }
+        }
+        private string _name = "";
+
+        public string Email
+        {
+            get { return _email ?? ""; }
+            set { _email = value ?? ""; }
+        }
+        private string _email = "";
+
+    }
+}
---
+++ Labs/Lab3/ContactManager.UI/ContactManager.UI/ContactManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ContactManager.UI
+{
+    public partial class ContactManager : Form
+    {
+        public ContactManager()
+        {
+            InitializeComponent();
+        }
+
+        private void OnFileExit(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void OnHelpAbout(object sender, EventArgs e)
+        {
+            var form = new AboutBox();
+            form.ShowDialog();
+        }
+    }
+}
